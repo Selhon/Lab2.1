691fc846f73e39facb20d4f29c26a4800e3cb75f
Selhon <51131431+Selhon@users.noreply.github.com>
2020-03-11 17:23:34 +0300

    begin

---
+++ Circle.cs
@@ -0,0 +1,29 @@
+using System;
+public class Circle
+{
+    double rad;
+    double x,y;
+    public Circle(double a,double b,double r){
+        rad=r;
+        x=a;
+        y=b;
+    }
+    public double Area (){
+        return Math.PI*rad*rad;
+    }
+    public double Perim (){
+        return 2*Math.PI*rad;
+    }
+    
+    
+    public double Radius (){
+        return rad;
+    }
+
+    
+    public void Move (double a,double b){
+        x=a;
+        y=b;
+    }
+
+}
